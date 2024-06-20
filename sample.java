64f88bbc50d113ae5905587b4c8dffaa648c1df3
ramagirirohith <ramagirirohith123@gmail.com>
2024-06-20 16:40:35 +0000

    anotherfunction code added

--- sample.java
+++ sample.java
@@ -1,6 +1,9 @@
 public static void main() {
 	system.out.println("Welcome to Teks Academy"):
 }
-void main() {
+void somefunction() {
         system.out.println("This is from some function"):
+}
+void anotherfunction() {
+        system.out.println("This is from another function"):
 }
