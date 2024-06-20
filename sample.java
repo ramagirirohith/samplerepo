a4f07de08f683036fc1dddef93f1dff7cb9d52a4
ramagirirohith <ramagirirohith123@gmail.com>
2024-06-20 17:02:09 +0000

    function one added

--- sample.java
+++ sample.java
@@ -1,9 +1,12 @@
 public static void main() {
-	system.out.println("Welcome to Teks Academy"):
+	system.out.println("Welcome to Teks Academy");
 }
 void somefunction() {
-        system.out.println("This is from some function"):
+        system.out.println("This is from some function");
 }
 void anotherfunction() {
-        system.out.println("This is from another function"):
+        system.out.println("This is from another function");
+}
+void functionone() {
+        system.out.println("This is from functionone");
 }
